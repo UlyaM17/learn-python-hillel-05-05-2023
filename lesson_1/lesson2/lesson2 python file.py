605489b8cd36839c793fbe2b94bdb68d764a4358
UlyanM17 <forsoc310@gmail.com>
2023-05-13 13:47:03 -0500

    check up

---
+++ lesson_1/lesson2/lesson2 python file.py
@@ -0,0 +1,21 @@
+a = 5
+b = a+3
+print(b)
+print(id(b))
+
+print(type(a))
+print(a ** b)
+
+c = b == a+3  # c = bool, logical statement, true or false
+
+print(c)
+first = True
+second = False
+
+print(first == second)
+
+import this
+
+
+
+
