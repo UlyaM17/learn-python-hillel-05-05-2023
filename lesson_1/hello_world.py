5b334a69b1b6ab04a6acacdb5ba12e3b59ff36b9
UlyanM17 <forsoc310@gmail.com>
2023-05-13 11:58:02 -0500

    check up

--- lesson_1/hello_world.py
+++ lesson_1/hello_world.py
@@ -1,4 +1,16 @@
-clone
-commit
-push
-pull
+# literals - any values,not changeable, ex. 5, -3. hellow world
+
+
+print(5)
+
+print("hello world")
+# changeable - space in memory, called as the programmer want ot to be
+
+x=5
+print(x)
+
+name = input('what is your name?')
+print('Hello', name)
+print('Hello, name')
+print(f'Hello'{name})
+
