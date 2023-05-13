235391e5b4c6292df7e4615340bceffad9db9303
UlyanM17 <forsoc310@gmail.com>
2023-05-13 15:06:35 -0500

    check up

--- lesson_1/lesson2/Homework #1.py
+++ lesson_1/lesson2/Homework #1.py
@@ -1,17 +1,29 @@
-gas_liter_for_100_km
-price_per_liter
-distance
+import decimal
 
 # gas used by average car for 100 km = 7l
 # price per l of gas = 46.50 UAH
 # distance = 100 km
 
-a = 7
-b = 46.50
+a = 7.000
+b = 46.500
 c = 100
 
 x = a * b
 print(x)
+
+round(x, 2)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
