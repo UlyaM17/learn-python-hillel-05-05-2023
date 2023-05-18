5785a57d2216338dba3e209aa7043f7a6b39c3f9
UlyanM17 <forsoc310@gmail.com>
2023-05-17 20:25:57 -0500

    check up

---
+++ lesson_1/lesson 3 file/Homework #2.py
@@ -0,0 +1,56 @@
+text = input("Hello, my favorite color is blue. What about you?")
+print(text)
+
+# lower case phrase
+print(str.lower(text))
+
+# omitting punctuation symbols
+punctuation = ('?', '.', ',', '!', ':', ';', '-')
+for objects in text:
+    text = text.replace('?', ' ')
+    text = text.replace('.', ' ')
+    text = text.replace(',', ' ')
+print(text)
+
+# omitting spaces
+text = text.replace(" ", "")
+print(text)
+
+# replacing a word
+sentence = input(text)
+word = input("favorite")
+replacement = input("most-liked")
+text2 = text.replace('favorite', 'most-liked')
+print(text2)
+
+# find the word meant to be replaced
+index = text.find("favorite")
+if text.find("favorite"):
+    print("success")
+
+# specific string
+word = 'favorite'
+result = word.find('favorite')
+print("Word 'favorite ' found at index:", result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
