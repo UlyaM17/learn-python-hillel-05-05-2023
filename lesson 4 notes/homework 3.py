c9109fe3d2a4e3b29a1f572f88d54c5189dbb87f
UlyanM17 <forsoc310@gmail.com>
2023-05-20 23:18:01 -0500

    check up

--- lesson 4 notes/homework 3.py
+++ lesson 4 notes/homework 3.py
@@ -1,30 +1,37 @@
-greeting = input('Hi, how are you?')
-print(greeting)
+print()
+response = ''
+response2 = ''
+activity = ''
+conclusion = ''
 
+greeting = input('Hello')
 response = str(input())
-if response == ('I am good'):
-    print("Great to hear that! What do you do?")
+if 'Hi' in response:
+    print('How are you?')
+elif 'Hello' in response:
+    print('How are you?')
 else:
-    if response == ('I am not good'):
-        print("Sorry to hear that. What do you do?")
-    else:
-        print('Sorry, I do not understand your answer')
-activity = str(input())
+    print('Hello?')
 
-if activity == ('I am learning Python'):
+response2 = str(input())
+if "good" in response2:
+    print("Great to hear that! What are you doing right now?")
+elif "well" in response2:
+    print("Great to hear that! What are you doing right now?")
+else:
+    print('Sorry, I do not understand your answer')
+
+activity = str(input())
+if "Python" in activity:
     print('It must be hard. Here is a helpful link: https://peps.python.org/pep-0008/#indentation')
 else:
-    if activity == ('I am watching videos on YouTube'):
+    if "video" in activity:
         print('It must be interesting! Here is a link to some interesting facts about Youtube: https://fortunelords.com/youtube-statistics/ ')
     else:
         print('Please rephrase your response, I do not understand')
 
 conclusion = str(input())
-
-if conclusion == ('Thank you'):
+if "thank" in conclusion:
     print('You are welcome. Good luck')
 else:
     print('Sorry that I could not help you.Please try again')
-
-
-
