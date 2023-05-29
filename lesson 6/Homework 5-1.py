f5d8165de49b3c33f9cd7d8c461a428241ec6eba
UlyanM17 <forsoc310@gmail.com>
2023-05-29 14:18:28 -0500

    check up

---
+++ lesson 6/Homework 5-1.py
@@ -0,0 +1,21 @@
+# program is asking for a word to be entered
+print('Please input your word')
+# user answers
+x = input()
+w = ''
+punctuation = ',./<>()-+=:;" '
+# reverse x:
+# x = x[::-1]
+# x = x.strip(punctuation)
+for i in x:
+    # lower case
+    x = x.lower()
+    # discard punctuation if any
+    for punctuation_symbol in punctuation:
+        x = x.replace(punctuation_symbol, '')
+if x == x[::-1]:
+    # is a palindrome
+    print(x, 'is a palindrome')
+else:
+    # is not
+    print(x, 'is not a palindrome')
