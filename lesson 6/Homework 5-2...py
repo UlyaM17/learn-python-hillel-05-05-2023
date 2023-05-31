793d59142abd5be812018b85c4c01c8629704f0a
UlyanM17 <forsoc310@gmail.com>
2023-05-31 07:36:05 -0500

    check up

--- lesson 6/Homework 5-2...py
+++ lesson 6/Homework 5-2...py
@@ -1,30 +1,17 @@
 # need to delete the content in '()'
-x = 'Today it was a (very) beautiful day))'
+x = 'Today it was a (very) beautiful day)).'
 punctuation = '()'
+what_we_seek = '('
+i = x.find(what_we_seek)
+new_x = ''
 # to remove text inside parenthesis
-for punctuation_symbol in punctuation:
-    # removing each character and the word
-    x = x.replace('(', '').replace(')', '').replace('very', '')
-    print(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while i != -1:
+    print(i)
+    i = x.find(what_we_seek, i + 1)
+    if what_we_seek == '(' and i != -1:
+        what_we_seek = ')'
+    elif what_we_seek == ')' and i != -1:
+        what_we_seek = '('
+new_x = x[:15] + x[22:35]
+print(new_x)
 
