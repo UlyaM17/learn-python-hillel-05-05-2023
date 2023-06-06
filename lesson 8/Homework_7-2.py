c7336c1946f9f0fb55c855e35f944d67b0261b02
UlyanM17 <forsoc310@gmail.com>
2023-06-06 00:47:00 -0500

    check up

--- lesson 8/Homework_7-2.py
+++ lesson 8/Homework_7-2.py
@@ -2,23 +2,24 @@ prime_set = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
 
 
 def is_prime(user_prompt):
-    while True:
-        number = input(f'{user_prompt}\n>>')
-        try:
-            if number in prime_set:
-                return True
-        except Exception:
-            # if value entered is not in a set
-            print(f'\nPlease try again')
+    number = input(f'{user_prompt}\n>>')
+    try:
+        # checking if entered number is in given set
+        if number in prime_set:
+            return True
+    except Exception:
+        # if value entered is not in a set
+        print(f'\nPlease try again')
+        return False
 
 
 if __name__ == '__main__':
     print(type(is_prime), is_prime)
     while True:
         # number was found in given set
-        n = is_prime('Please enter a numeric value from 1 till 100:\n>')
+        n = is_prime('Please enter a numeric value from 1 till 100:\n')
         prime_set = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
-        if is_prime() is True:
+        if is_prime(user_prompt=n) is True:
             # conclusion
             print(f'Your number is prime')
 
