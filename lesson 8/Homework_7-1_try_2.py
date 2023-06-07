8f78fdfbdf68ba19a7b881584f80cb23246b24cc
UlyanM17 <forsoc310@gmail.com>
2023-06-07 10:19:46 -0500

    check up

--- lesson 8/Homework_7-1_try_2.py
+++ lesson 8/Homework_7-1_try_2.py
@@ -1,15 +1,15 @@
-def read_user_number(user_prompt):
+def read_user_number(user_prompt) -> int:
     """
             function checks if the number is numeric and whole
             :param user_prompt: user entry
             :return: yes, not
             """
     while True:
+        user_input = input(f'{user_prompt}\n>> ')
         try:
-            user_input = input(f'{user_prompt}\n>> ')
+            user_input = int(user_input)
             # is a whole number
-            if user_input == int(user_input):
-                return user_input
+            return user_input
         except Exception:
             # if decimal/ negative number entered
             print(f'\nPlease enter a whole numeric value')
@@ -20,15 +20,13 @@ def convert_user_number(user_input) -> dict:
             :param: user_input
             :return: dictionary
             """
-    while user_input is True:
-        d = dict()
-        if d:
-            d = {'seconds': {user_input},
-                 'minutes': divmod(user_input, 60),
-                 'hours': divmod(user_input, (60 * 60)),
-                 'days': divmod(user_input, (24 * 60 * 60))
-                 }
-            return d
+    if type(user_input) == int:
+        d = {'seconds': {user_input},
+             'minutes': divmod(user_input, 60),
+             'hours': divmod(user_input, (60 * 60)),
+             'days': divmod(user_input, (24 * 60 * 60))
+             }
+        return d
     else:
         print(f'\nCould not calculate')
 
@@ -38,5 +36,3 @@ if __name__ == '__main__':
         seconds_entry = read_user_number(user_prompt='Enter number of seconds\n>> ')
         seconds_converted = convert_user_number(seconds_entry)
         print(seconds_converted)
-
-
