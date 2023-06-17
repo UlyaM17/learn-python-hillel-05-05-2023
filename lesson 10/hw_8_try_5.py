d08ef94e5874c9cb729b757a60d47b3724018d91
UlyanM17 <forsoc310@gmail.com>
2023-06-17 13:10:06 -0500

    check up

--- lesson 10/hw_8_try_5.py
+++ lesson 10/hw_8_try_5.py
@@ -1,60 +1,67 @@
-my_list = []
 
-
-def key_word_add(keyword_add: str) -> list:
+def key_word_add(keyword_input) -> list:
     """
     :rtype: list
     """
+    my_list = []
     while True:
-        keyword_input = input('>>\n>>')
-        if 'add' in keyword_input:
-            return [my_list.append(keyword_add)]
+        user_input = input(f'{keyword_input}\n>>')
+        if 'add' in user_input:
+            return [my_list.append(user_input)]
 
 
-def key_word_earliest(keyword_input: list) -> list:
+def key_word_earliest(user_input: list) -> list:
     """
-    :rtype: list
+    :param user_input
+    :return: list
     """
-    if 'earliest' in keyword_input:
-        return [my_list]
+    if 'earliest' in user_input:
+        return [user_input.append(user_input)]
 
 
-def key_word_latest(keyword_input: list) -> list:
+def key_word_latest(user_input: list) -> list:
     """
-    :rtype: list
+    :param user_input
+    :return: list
     """
-    if 'latest' in keyword_input:
-        return [my_list[::-1]]
+    if 'latest' in user_input:
+        return user_input.append(user_input)[::-1]
 
 
-def key_word_shortest(keyword_input: list) -> list:
+def key_word_shortest(user_input: list) -> list:
     """
-    :rtype: list
+    :param user_input
+    :return: list
     """
-    if 'shortest' in keyword_input:
-        return [sorted(my_list, key=len)]
+    if 'shortest' in user_input:
+        return sorted(user_input, key=len)
 
 
-def key_word_longest(keyword_input: list) -> list:
+def key_word_longest(user_input: list) -> list:
     """
-    :rtype: list
+    :param user_input
+    :return: list
     """
-    if 'longest' in keyword_input:
-        return [sorted(my_list, key=len, reverse=True)]
+    if 'longest' in user_input:
+        return sorted(user_input, key=len, reverse=True)
 
 
 if __name__ == '__main__':
     while True:
-        notes_completed = key_word_add(keyword_add='Please add note or input command: add, earliest, latest, shortest '
-                                                   'or'
-                                                   'longest \n>>')
-        print(notes_completed)
-        earliest_entry = key_word_earliest(notes_completed)
-        print(earliest_entry)
-        latest_entry = key_word_latest(notes_completed)
-        print(latest_entry)
-        shortest_entry = key_word_shortest(notes_completed)
-        print(shortest_entry)
-        longest_entry = key_word_longest(notes_completed)
-        print(longest_entry)
+        user_command = key_word_add(keyword_input='Please add note or input command: add, earliest, latest, shortest '
+                                                  'or'
+                                                  'longest \n>>')
+        print(user_command)
+        earliest_entry = key_word_earliest(user_command)
+        if earliest_entry:
+            print(earliest_entry)
+        latest_entry = key_word_latest(user_command)
+        if latest_entry:
+            print(latest_entry)
+        shortest_entry = key_word_shortest(user_command)
+        if shortest_entry:
+            print(shortest_entry)
+        longest_entry = key_word_longest(user_command)
+        if longest_entry:
+            print(longest_entry)
 
