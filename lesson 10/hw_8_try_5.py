9946091c5f7b4ccc0462c5dd8a529dddb5acd6fc
UlyanM17 <forsoc310@gmail.com>
2023-06-18 14:31:24 -0500

    check up

--- lesson 10/hw_8_try_5.py
+++ lesson 10/hw_8_try_5.py
@@ -1,61 +1,71 @@
-def word_add(keyword_input) -> list:
+def word(keyword_input: str) -> list:
     """
     :rtype: list
     """
     # my_list = []
     while True:
         user_input = input(f'{keyword_input}\n>>')
-        if 'add' in user_input:
-            user_list = user_input
-            return [user_list]
+        try:
+            user_list = [user_input]
+            return user_list
+        except Exception:
+            print('Try again')
 
 
-def key_word_earliest(user_input: list) -> list:
+def key_word_add(user_list: list) -> list:
     """
-    :param user_input
+    :param user_list
     :return: list
     """
-    if 'earliest' in user_input:
-        user_list = user_input
-        return [user_list.append(user_input)]
+    if 'add' in user_list:
+        return [user_list.append(user_list)]
 
 
-def key_word_latest(user_input: list) -> list:
+def key_word_earliest(user_list: list) -> list:
     """
-    :param user_input
+    :param user_list
     :return: list
     """
-    if 'latest' in user_input:
-        user_list = user_input
-        return user_list.append(user_input)[::-1]
+    if 'earliest' in user_list:
+        return [user_list.append(user_list)]
 
 
-def key_word_shortest(user_input: list) -> list:
+def key_word_latest(user_list: list) -> list:
     """
-    :param user_input
+    :param user_list
     :return: list
     """
-    if 'shortest' in user_input:
-        user_list = user_input
+    if 'latest' in user_list:
+        return user_list.append(user_list)[::-1]
+
+
+def key_word_shortest(user_list: list) -> list:
+    """
+    :param user_list
+    :return: list
+    """
+    if 'shortest' in user_list:
         return sorted(user_list, key=len)
 
 
-def key_word_longest(user_input: list) -> list:
+def key_word_longest(user_list: list) -> list:
     """
-    :param user_input
+    :param user_list
     :return: list
     """
-    if 'longest' in user_input:
-        user_list = user_input
+    if 'longest' in user_list:
         return sorted(user_list, key=len, reverse=True)
 
 
 if __name__ == '__main__':
     while True:
-        user_command = word_add(keyword_input='Please input a command: add, earliest, latest, shortest '
-                                              'or '
-                                              'longest \n>>')
+        user_command = word(keyword_input='Please input a command: add, earliest, latest, shortest '
+                                          'or '
+                                          'longest \n>>')
         print(user_command)
+        if user_command:
+            add_entry = key_word_add(user_command)
+            print(add_entry)
         if user_command:
             earliest_entry = key_word_earliest(user_command)
             print(earliest_entry)
